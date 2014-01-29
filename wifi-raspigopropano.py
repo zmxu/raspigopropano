eb03d48908bf31688b2312c36c7820ef4b728fdd
Konrad Iturbe <github@chernowii.com>
2014-01-29 20:04:11 +0100

    Update wifi-raspigopropano.py

--- wifi-raspigopropano.py
+++ wifi-raspigopropano.py
@@ -5,3 +5,5 @@ import {stitch.py}
 import RPi.GPIO
 #replace PASSWORD by the camera wifi password
 {move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(2)
