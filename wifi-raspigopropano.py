1e2a4c06320cb6bbdeb2935a3ba6df3497014dce
Konrad Iturbe <github@chernowii.com>
2014-01-29 19:56:47 +0100

    Update wifi-raspigopropano.py

--- wifi-raspigopropano.py
+++ wifi-raspigopropano.py
@@ -1,1 +1,7 @@
 # Raspberry PI GoPro Pano WiFi option Python file
+import time
+import urllib2
+import {stitch.py}
+import RPi.GPIO
+#replace PASSWORD by the camera wifi password
+{move servo 45 degrees}
