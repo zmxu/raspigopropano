2ed08b2ab3dc1f836dedf3231b88e5160e5c1d07
Konrad Iturbe <github@chernowii.com>
2014-01-29 20:12:13 +0100

    Update wifi-raspigopropano.py

--- wifi-raspigopropano.py
+++ wifi-raspigopropano.py
@@ -7,3 +7,28 @@ import RPi.GPIO
 {move servo 45 degrees}
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
+{move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(2)
+{move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(2)
+{move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(2)
+{move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(2)
+{move servo 45 degrees}
+urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
+time.sleep(3)
+urllib2.urlretrieve("http://10.5.5.9:8080/videos/DCIM/XXXGOPRO") # XXX is the number of the folder your gopro creates
+-------------
+| Stitch:
+| SRC: usr/downloads
+| Target: usr/downloads/raspigopropano
+| Degrees: 270
+| Number of pics: 6
+-------------
+time.sleep(4)
+print("Pano finished, be a hero.")
