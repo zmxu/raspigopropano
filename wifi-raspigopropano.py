350779e9c8e6d2e39a829d405cd04c093f12c277
Konrad Iturbe <github@chernowii.com>
2014-01-30 18:11:45 +0100

    Update wifi-raspigopropano.py

--- wifi-raspigopropano.py
+++ wifi-raspigopropano.py
@@ -3,23 +3,43 @@ import time
 import urllib2
 import {stitch.py}
 import RPi.GPIO
+def set(property, value):
+ try:
+ f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+ f.write(value)
+ f.close() 
+ except:
+ print("Error writing to: " + property + " value: " + value)
+def setServo(angle):
+ set("servo", str(angle))
+set("delayed", "0")
+set("mode", "servo")
+set("servo_max", "180")
+set("active", "1")
+delay_period = 0.01
 #replace PASSWORD by the camera wifi password
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(2)
-{move servo 45 degrees}
+for angle in range(0, 45):
+setServo(45)
 urllib2.urlopen("http://10.5.5.9/camera/SH?t=PASSWORD&p=%02")
 time.sleep(3)
 urllib2.urlretrieve("http://10.5.5.9:8080/videos/DCIM/XXXGOPRO") # XXX is the number of the folder your gopro creates
